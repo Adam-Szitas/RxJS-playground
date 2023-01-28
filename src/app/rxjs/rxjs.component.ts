2788629b600e2a127bde4536f1e0fcad88851a00
Adam-Szitas <example.eu.sk.number@gmail.com>
2023-01-28 22:42:27 +0100

    [28.01.2023] Updating rjxs-playground

---
+++ src/app/rxjs/rxjs.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { concatMap, delay, exhaustMap, from, mergeMap, Observable, of, switchMap } from 'rxjs';
+
+@Component({
+  selector: 'app-rxjs',
+  templateUrl: './rxjs.component.html',
+  styleUrls: ['./rxjs.component.scss']
+})
+export class RxjsComponent implements OnInit {
+
+  public result: string = 'string';
+  public form: FormGroup = new FormGroup({});
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.form = new FormGroup({
+      username: new FormControl()
+    });
+
+    const obsOne: Observable<string[]> = of(["first","second", "third"]);
+
+    const obsTwo: Observable<string[]> = of(["fourth", "fifth", "sixth"]);
+
+    console.log('Simple echo');
+    obsOne.subscribe(
+      data => console.log(data)
+      )
+
+    console.log('concatMap');
+    this.form.valueChanges.pipe(
+      concatMap(formValue => of(formValue).pipe(delay(1000)))
+      ).subscribe(
+        data => console.log(data)
+        )
+
+    console.log('switchMap');
+    this.form.valueChanges.pipe(
+      switchMap(formValue => of(formValue).pipe(delay(1000)))
+      ).subscribe(
+        data => console.log(data)
+        )
+
+    console.log('mergeMap');
+    this.form.valueChanges.pipe(
+      mergeMap(formValue => of(formValue).pipe(delay(1000)))
+      ).subscribe(
+        data => console.log(data)
+        )
+
+    console.log('exhaustMap');
+    this.form.valueChanges.pipe(
+      exhaustMap(formValue => of(formValue).pipe(delay(1000)))
+    ).subscribe(
+      data => console.log(data)
+    )
+  }
+}
